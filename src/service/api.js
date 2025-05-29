42ec04ee6134cc4f9ef207bf6da58e107cbf3b05
vjamespd <dvinardjames@gmail.com>
2025-05-29 17:35:03 +0800

    API BASE COMMIT

--- src/service/api.js
+++ src/service/api.js
@@ -5,7 +5,7 @@ const API_BASE = import.meta.env.VITE_API_BASE;
 export const uploadCSV = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
-  return fetch(`{API_BASE}upload/`, {
+  return fetch(`${API_BASE}/upload/`, {
     method: "POST",
     body: formData,
   });
